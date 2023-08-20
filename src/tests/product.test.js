e77baa2908959c95687ff6f823e5e5fb2a76e934
millos910 <emontero910@gmail.com>
2023-08-20 17:47:44 -0500

    testing products pasado

--- src/tests/product.test.js
+++ src/tests/product.test.js
@@ -8,6 +8,7 @@ const URL_BASE_USERS="/api/v1/users/login"
 let TOKEN
 let product
 let category
+let productId
 beforeAll(async()=>{
     const user={
         email: "emontero910@gmail.com",
@@ -33,9 +34,59 @@ test("POST -> 'URL_BASE',should return code 201 and res.body.title === product.t
         .post(URL_BASE)
         .send(product)
         .set("Authorization", `Bearer ${TOKEN}`)
-
+    productId=res.body.id
     expect(res.status).toBe(201)
     expect(res.body).toBeDefined()
     expect(res.body.title).toBe(product.title)
-await category.destroy()
+
 })
+
+test("GET ->'URL_BASE', should return staus code 200 and res.body.length === 1", async () => {      
+    const res = await request(app)
+        .get(URL_BASE)
+
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveLength(1)
+})
+
+test("GET ONE->'URL_BASE/:id', should return staus code 200 and res.body.title === product.title", async () => {      
+    const res = await request(app)
+        .get(`${URL_BASE}/${productId}`)
+
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.title).toBe(product.title)
+})
+
+test("PUT->'URL_BASE/:id', should return staus code 200 and res.body.title === productUpdate.title", async () => {      
+    const productUpdate={
+        title:'Donnuts spiderman'
+    }
+    
+    const res = await request(app)
+        .put(`${URL_BASE}/${productId}`)
+        .send(productUpdate)
+        .set("Authorization", `Bearer ${TOKEN}`)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.title).toBe(productUpdate.title)
+})
+
+test("Delete->'URL_BASE/:id', should return staus code 204 ", async () => {      
+    const res = await request(app)
+        .delete(`${URL_BASE}/${productId}`)
+        .set("Authorization", `Bearer ${TOKEN}`)
+
+    expect(res.status).toBe(204)
+    await category.destroy()
+})
+
+
+
+
+
+
